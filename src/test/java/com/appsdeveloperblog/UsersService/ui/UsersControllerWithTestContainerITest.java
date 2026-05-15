633c4ce36b4f60fa755fdc864ba046e68f8f77de
parom <E.Parominskiy@isida.by>
2026-05-15 15:04:00 +0300

    146. Request and Response Logging filters in REST Assured

--- src/test/java/com/appsdeveloperblog/UsersService/ui/UsersControllerWithTestContainerITest.java
+++ src/test/java/com/appsdeveloperblog/UsersService/ui/UsersControllerWithTestContainerITest.java
@@ -3,6 +3,9 @@ package com.appsdeveloperblog.UsersService.ui;
 import com.appsdeveloperblog.UsersService.ui.model.User;
 import com.appsdeveloperblog.UsersService.ui.model.UserRest;
 import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
@@ -41,11 +44,18 @@ public class UsersControllerWithTestContainerITest {
     @LocalServerPort
     private int port;
 
+    private final RequestLoggingFilter requestLoggingFilter =
+//            new RequestLoggingFilter();
+                RequestLoggingFilter.with(LogDetail.BODY, LogDetail.HEADERS);
+
+    private final ResponseLoggingFilter responseLoggingFilter = new ResponseLoggingFilter();
+
     @BeforeAll
     void setUp() {
         RestAssured.baseURI = "http://localhost";
 //        RestAssured.port=8080;
         RestAssured.port = this.port;
+        RestAssured.filters(requestLoggingFilter, responseLoggingFilter);
     }
 
     @Order(1)
@@ -110,7 +120,7 @@ public class UsersControllerWithTestContainerITest {
 
         // Act
         given()
-                .log().all()
+//                .log().all() // log request info use requestLoggingFilter instead!
                 .headers(headers)
                 .body(newUser)
         .when()
